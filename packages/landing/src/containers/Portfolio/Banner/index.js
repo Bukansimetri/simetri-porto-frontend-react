792edd01fbe43052a470840ced20022d56f8e430
Jumraty Ahmad <jumratyahmad@MacBook-Air-Jumraty.local>
2025-07-05 21:42:02 +0700

    First commit

---
+++ packages/landing/src/containers/Portfolio/Banner/index.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Box from 'common/components/Box';
+import Text from 'common/components/Text';
+import Heading from 'common/components/Heading';
+import NextImage from 'common/components/NextImage';
+import Container from 'common/components/UI/Container';
+import BannerWrapper from './banner.style';
+
+import PersonImage from 'common/assets/image/portfolio/person.png';
+
+const BannerSection = ({
+  row,
+  contentArea,
+  imageArea,
+  nameStyle,
+  aboutStyle,
+}) => {
+  return (
+    <BannerWrapper id="banner_section">
+      <Container noGutter mobileGutter width="1200px">
+        <Box {...row}>
+          <Box {...contentArea}>
+            <Heading content="Crafting Balanced Digital Experiences" {...nameStyle} />
+            <Text
+              content="Kami membangun solusi digital yang tidak hanya berfungsi, tapi juga terasa tepat secara visual dan teknis. Ingin tahu solusi terbaik untuk bisnis Anda? Konsultasi bersama kami gratis, tanpa komitmen."
+              {...aboutStyle}
+            />
+          </Box>
+          <Box {...imageArea} className="image_area">
+            <NextImage src={PersonImage} alt="Simetri" />
+          </Box>
+        </Box>
+      </Container>
+    </BannerWrapper>
+  );
+};
+
+BannerSection.propTypes = {
+  row: PropTypes.object,
+  contentArea: PropTypes.object,
+  imageArea: PropTypes.object,
+  greetingStyle: PropTypes.object,
+  nameStyle: PropTypes.object,
+  designationStyle: PropTypes.object,
+  aboutStyle: PropTypes.object,
+  roleStyle: PropTypes.object,
+  roleWrapper: PropTypes.object,
+};
+
+BannerSection.defaultProps = {
+  row: {
+    flexBox: true,
+    flexWrap: 'wrap',
+    alignItems: 'stretch',
+  },
+  contentArea: {
+    width: ['100%', '100%', '50%', '40%'],
+    p: ['65px 0 80px 0', '65px 0 80px 0', '80px 0 60px 0', '0'],
+    flexBox: true,
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    flexDirection: 'column',
+  },
+  imageArea: {
+    width: ['100%', '100%', '50%', '60%'],
+    flexBox: true,
+    alignItems: 'flex-end',
+  },
+  greetingStyle: {
+    as: 'h3',
+    color: '#fff',
+    fontSize: ['18px', '18px', '18px', '20px', '30px'],
+    fontWeight: '500',
+    mb: '8px',
+  },
+  nameStyle: {
+    as: 'h2',
+    color: '#fff',
+    fontSize: ['38px', '38px', '44px', '60px', '80px'],
+    fontWeight: '800',
+    mb: '6px',
+  },
+  designationStyle: {
+    as: 'h3',
+    color: '#fff',
+    fontSize: ['18px', '18px', '18px', '20px', '30px'],
+    fontWeight: '700',
+    mb: ['30px', '30px', '25px', '30px', '30px'],
+  },
+  roleWrapper: {
+    flexBox: true,
+    mb: '28px',
+  },
+  roleStyle: {
+    as: 'h4',
+    fontSize: ['18px', '18px', '18px', '18px', '20px'],
+    fontWeight: '500',
+    color: '#fff',
+    mb: '0',
+    ml: '10px',
+  },
+  aboutStyle: {
+    fontSize: ['15px', '15px', '15px', '16px', '16px'],
+    fontWeight: '400',
+    color: '#fff',
+    lineHeight: '1.5',
+    mb: '50px',
+  },
+};
+
+export default BannerSection;
